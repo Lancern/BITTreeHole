fdd90162f517fdffd63abc2dacda5a69c266ee93
Lancern <msrlancern@126.com>
2019-05-15 22:46:23 +0800

    [feat] Logging utilities

--- BITTreeHole/Program.cs
+++ BITTreeHole/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace BITTreeHole
 {
@@ -19,6 +21,12 @@ namespace BITTreeHole
                        config.SetBasePath(Directory.GetCurrentDirectory());
                        config.AddJsonFile("AppCriticalSettings.json", optional: true, reloadOnChange: false);
                    })
+                   .ConfigureLogging(builder =>
+                   {
+                       builder.ClearProviders();
+                       builder.SetMinimumLevel(LogLevel.Trace);
+                   })
+                   .UseNLog()
                    .UseStartup<Startup>();
     }
 }
